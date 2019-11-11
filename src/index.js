d412069602374a8e604f38f4efa7cf6b23d7399b
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-11 11:14:38 +0100

    add under construction text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello, World!</h1>, document.querySelector('#root'));
+ReactDOM.render(<h1>Under construction!</h1>, document.querySelector('#root'));
