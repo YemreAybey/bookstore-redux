685539aef6d063165ab4664cb237e8ec5145b7a8
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-12 15:39:58 +0100

    implement bookslist component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class BookList extends React.Component {
-  render() {
-    return <div>BooksList</div>;
-  }
-}
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </thead>
+    <tbody>
+      {books.map((book) => (
+        <tr key={book.id}>
+          <td>book.id</td>
+          <td>books.title</td>
+          <td>book.category</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
-export default BookList;
+const mapStateToProps = ({ books }) => ({ books });
+
+const matDispatchToProps = () => {};
+
+BooksList.prototype = {
+  book: PropTypes.array,
+};
+
+export default connect(mapStateToProps, matDispatchToProps)(BooksList);
