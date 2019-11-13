f046788ea70e52157d6ca4564281c3e107b8ce7c
Yemre <aybey77@gmail.com>
2019-11-13 19:29:44 +0300

    Connect removeBook action to Booklist and book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-const Book = ({ data }) => (
+const Book = ({ data, removeBook }) => (
   <tr>
     <td>{data.id}</td>
     <td>{data.title}</td>
     <td>{data.category}</td>
+    <td>
+      <button
+        onClick={() => removeBook(data)}
+        type="button"
+      >
+        Delete
+      </button>
+    </td>
   </tr>
 );
 
 Book.propTypes = {
+  removeBook: PropTypes.func.isRequired,
   data: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
