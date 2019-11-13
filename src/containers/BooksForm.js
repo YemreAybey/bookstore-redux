710d0906ca092af2ccef837e0e695f2da95fd60a
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-13 16:50:33 +0100

    connect BookForm to store dispatch

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Form from '../components/Form';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { createBook } from '../actions';
+import idGen from '../utils/idGenerator';
 
 const CATEGORIES = [
   'Action',
@@ -11,15 +14,81 @@ const CATEGORIES = [
   'Sci-Fi',
 ];
 
-const handleFormSubmit = () => {
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: CATEGORIES[0],
+    };
+
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+
+  handleInputChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  handleSelect(e) {
+    this.setState({ category: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { title, category } = this.state;
+    const { createBook } = this.props;
+    const book = {
+      id: idGen(),
+      title,
+      category,
+    };
+    this.setState({
+      title: '',
+      category: CATEGORIES[0],
+    });
+    createBook(book);
+  }
+
+  render() {
+    const { title, category } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={this.handleInputChange}
+        />
+
+        <select
+          value={category}
+          onChange={this.handleSelect}
+        >
+          {CATEGORIES.map(c => (
+            <option
+              key={c}
+              value={c}
+            >
+              {c}
+            </option>
+          ))}
+        </select>
+        <input type="submit" value="Save the Book" />
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  createBook(book) { dispatch(createBook(book)); },
+});
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
 };
 
-const BooksForm = () => (
-  <Form
-    placeholder="Book Title"
-    options={CATEGORIES}
-    submitHandler={handleFormSubmit}
-  />
-);
-
-export default BooksForm;
+export default connect(null, mapDispatchToProps)(BooksForm);
